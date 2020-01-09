1a7d0bf9fbed1b0b51369508f2b10a19ff3b0c55
brittanieb <brittanie.boyko@gmail.com>
2020-01-09 00:45:32 -0500

    removed callback in script tag

--- iss.js
+++ iss.js
@@ -1,36 +1,42 @@
 
-    function initMap() {
-    
-    var queryURL = "http://api.open-notify.org/iss-now.json";
-    var map;
-    var shuttleIcon = "assets/images/shuttle.png"
+$(document).ready(function(){
+    function positionMarker(location, map) {
+        var shuttleIcon = "assets/images/shuttle.png"
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    
-    .then (function (data) {
-        console.log(data)
-
-        var lat = parseInt(data.iss_position.latitude);
-        var lon = parseInt(data.iss_position.longitude);
-        var iss = {lat: lat, lng: lon};
-    
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: iss,
-            gestureHandling: 'none',
-            zoomControl: false,
-            zoom: 2
-        });
-    
         var marker = new google.maps.Marker({
-            position: iss,
+            position: location,
             icon: shuttleIcon,
             map: map
         });
-        
-        setTimeout(initMap, 5000);
-    });
-  
     }
+
+    function initMap() {
+        
+        var queryURL = "http://api.open-notify.org/iss-now.json";
+        var map;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        
+        .then (function (data) {
+            var lat = parseInt(data.iss_position.latitude);
+            var lon = parseInt(data.iss_position.longitude);
+            var iss = {lat: lat, lng: lon};
+        
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: iss,
+                gestureHandling: 'none',
+                zoomControl: false,
+                zoom: 2
+            });
+
+            positionMarker(iss, map)
+        });
+
+        setTimeout(initMap, 5000);
+    }
+
+    initMap();
+});
