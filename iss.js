d079fa3803e4010a0092af091ed50de34b4aba7f
brittanieb <brittanie.boyko@gmail.com>
2020-01-08 23:51:35 -0500

    grab iss coordinates and place google maps marker at that location

--- iss.js
+++ iss.js
@@ -1,6 +1,8 @@
 
+    function initMap() {
+    
     var queryURL = "http://api.open-notify.org/iss-now.json";
-
+    var map;
 
     $.ajax({
         url: queryURL,
@@ -9,12 +11,21 @@
     
     .then (function (data) {
         console.log(data)
+
+        var lat = parseInt(data.iss_position.latitude);
+        var lon = parseInt(data.iss_position.longitude);
+        var iss = {lat: lat, lng: lon};
+    
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: iss,
+            zoom: 2
+        });
+    
+        var marker = new google.maps.Marker({
+            position: iss,
+            map: map
+        });
     });
 
-    var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 1
-      });
+  
     }
