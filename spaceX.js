b4bf9468fcca0d16679e454c69a8557bf4d9f49c
ian <ianwatson115@gmail.com>
2020-01-09 22:47:12 -0500

    added information from clickable buttons on to the screen

--- spaceX.js
+++ spaceX.js
@@ -1,35 +1,62 @@
 $(document).ready(function(){
 
-    var upcomingLaunchesURL = "https://api.spacexdata.com/v3/launches/upcoming";
+    var $displayDiv = $(".displayDiv");
+    
+    $("#upcomingLaunches").click(function() {
 
-    $.ajax({
-        url: upcomingLaunchesURL,
-        method: "GET"
+        var upcomingLaunchesURL = "https://api.spacexdata.com/v3/launches/upcoming";
+
+        $.ajax({
+            url: upcomingLaunchesURL,
+            method: "GET",
+        })
+
+        .then(function(upcomingLaunches) {
+
+            console.log(upcomingLaunches);
+
+            $.each(upcomingLaunches, function(i, data){
+                $displayDiv.append("<li>Mission Name: " + data.mission_name + " Launch Date: " + data.launch_date_utc + " </li>") 
+            })
+        })
     })
 
-    .then(function(data) {
-        console.log(data);
+    $("#missions").click(function() {
+
+        var missionsURL = "https://api.spacexdata.com/v3/missions";
+        
+    
+        $.ajax({
+            url: missionsURL,
+            method: "GET"
+        })
+
+        .then(function(missions) {
+            console.log(missions);
+
+            $.each(missions, function(i, data){
+                $displayDiv.append("<li>Mission Name: " + data.mission_name + " </li>" + "<li>Description: " + data.description + " </li>"); 
+            })
+        })
     })
 
-    var missionsURL = "https://api.spacexdata.com/v3/missions";
-   
-    $.ajax({
-        url: missionsURL,
-        method: "GET"
-    })
+    $("#rockets").click(function() {
 
-    .then(function(data) {
-        console.log(data);
-    })
+        var rocketsURL = "https://api.spacexdata.com/v3/rockets";
 
-    var rocketsURL = "https://api.spacexdata.com/v3/rockets";
+        $.ajax({
+            url: rocketsURL,
+            method: "GET"
+        })
 
-    $.ajax({
-        url: rocketsURL,
-        method: "GET"
-    })
-
-    .then(function(data) {
-        console.log(data);
+        .then(function(rockets) {
+            console.log(rockets);
+            
+            $.each(rockets, function(i, data){
+                $displayDiv.append("<img src=\"" + data.flickr_images[0] + "\">" + "<li> " + data.rocket_name + "</li>" + 
+                "<li>Mass: " + data.mass.kg + " kg </li>" + "<li>Height: " + data.height.meters + "m </li>" + "<li>Cost per launch $" + data.cost_per_launch  + "</li>"
+                + "<li>Description: " + data.description + "</li>");
+            })
+        })
     })
 })
