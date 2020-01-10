bb888997f12634a0190eb64a9f3c254e5ff1f83f
brittanieb <brittanie.boyko@gmail.com>
2020-01-09 23:03:28 -0500

    updated api key to grab APOD

--- nasa.js
+++ nasa.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-    var nasaURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=";
+    var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
+    
     var nasaKey = "wX95wmF1OaylDvqokfgXj5v4WftUvcM915KPIUv1";
 
     $.ajax({
