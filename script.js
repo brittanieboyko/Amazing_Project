65621a05062e3054f6ff31f71d861ba8759ce190
brittanieb <brittanie.boyko@gmail.com>
2020-01-11 13:45:44 -0500

    move image of the day functionality to script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-var queryURL = "https://api.spacexdata.com/v3/missions";
-
 $(document).ready(function(){
+    var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
+    var nasaKey = "wX95wmF1OaylDvqokfgXj5v4WftUvcM915KPIUv1";
+    var imageContainer = $(".apod-container");
+    var apodImage = $("<img>");
+    var apodTitle = $("<h4>");
+    var apodDate = $("<p>");
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-  
-  .then (function (data) {
-
-  console.log(data)
-  })
-
-});
-
-var queryURL = "http://api.open-notify.org/astros.json";
     $.ajax({
-        url: queryURL,
+        url: nasaURL + nasaKey,
         method: "GET"
-
     })
 
-    .then(function (data){
+    .then(function(data) {
+        apodImage.attr({
+            src: data.url,
+            alt: data.title
+            });
 
-console.log(data.number)
-    })
+        apodTitle.text(data.title);
+        apodDate.text(data.date);
+
+        imageContainer.append(apodImage);
+        imageContainer.append(apodTitle);
+        imageContainer.append(apodDate);
+    });
+});
