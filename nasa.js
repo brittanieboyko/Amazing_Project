2866af589b8d9622a29bd9a2ccbd8b0529df0b6a
brittanieb <brittanie.boyko@gmail.com>
2020-01-10 22:46:07 -0500

    call to nasa image library api

--- nasa.js
+++ nasa.js
@@ -1,25 +1,40 @@
 $(document).ready(function(){
 
-    var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
-    
-    var nasaKey = "wX95wmF1OaylDvqokfgXj5v4WftUvcM915KPIUv1";
-
-    $.ajax({
-        url: nasaURL + nasaKey,
-        method: "GET"
-    })
-
-    .then(function(data) {
-        console.log(data);
+    function getAPOD() {
+        var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
         
-        $("#apod-image").attr({
-            src: data.url,
-            alt: data.title
-          });
-        $("#apod-title").text(data.title);
-        $("#apod-date").text(data.date);
-    })
+        var nasaKey = "wX95wmF1OaylDvqokfgXj5v4WftUvcM915KPIUv1";
 
+        $.ajax({
+            url: nasaURL + nasaKey,
+            method: "GET"
+        })
 
+        .then(function(data) {
+            
+            $("#apod-image").attr({
+                src: data.url,
+                alt: data.title
+                });
+            $("#apod-title").text(data.title);
+            $("#apod-date").text(data.date);
+        });
+    }
+
+    getAPOD();
+
+    function getNasaImageLibrary() {
+        var nasaURL = "https://images-api.nasa.gov/search?q=orion";
+
+        $.ajax({
+            url: nasaURL,
+            method: "GET"
+        })
+
+        .then(function(data) {
+            console.log(data);
+        });
+    }
+    getNasaImageLibrary();
 
 })
