9038d6d5f0818ed91732e0c0b7f1affe6024e261
brittanieb <brittanie.boyko@gmail.com>
2020-01-13 16:34:18 -0500

    added modal functionality to apod at index.html

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 $(document).ready(function(){
-    var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
-    var nasaKey = "wX95wmF1OaylDvqokfgXj5v4WftUvcM915KPIUv1";
-    var imageContainer = $(".apod-container");
     var apodImage = $("<img>");
-    var apodTitle = $("<h4>");
-    var apodDate = $("<p>");
+    var modal = document.querySelector("#apod-modal");
+    var span = document.getElementsByClassName("close")[0];
+    var imageDescription = $(".modal-caption-text");
 
-    $.ajax({
-        url: nasaURL + nasaKey,
-        method: "GET"
-    })
+    function getAPODData() {
+        var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
+        var nasaKey = "wX95wmF1OaylDvqokfgXj5v4WftUvcM915KPIUv1";
 
-    .then(function(data) {
-        apodImage.attr({
-            src: data.url,
-            alt: data.title
-            });
+        var apodTitle = $("<h4>");
+        var apodDate = $("<p>");
+        var imageContainer = $(".apod-container");
 
-        apodTitle.text(data.title);
-        apodDate.text(data.date);
+        $.ajax({
+            url: nasaURL + nasaKey,
+            method: "GET"
+        })
+    
+        .then(function(data) {
+            apodImage.attr({
+                src: data.url,
+                alt: data.title
+                });
+    
+            apodTitle.text(data.title);
+            apodDate.text(data.date);
+            imageDescription.text(data.explanation);
+            console.log(imageDescription.text());
+    
+            imageContainer.append(apodImage);
+            imageContainer.append(apodTitle);
+            imageContainer.append(apodDate);
+        });
+    }
 
-        imageContainer.append(apodImage);
-        imageContainer.append(apodTitle);
-        imageContainer.append(apodDate);
-    });
+    function displayModal() {
+        var modalImg = document.querySelector(".modal-content");
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+    }
+
+    getAPODData();
+
+    apodImage.on("click", displayModal);
+    span.onclick = closeModal;
+
 });
